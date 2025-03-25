a4eb679d2e44ea34808d78f47771c263f5d30281
saqlain <saqlainabbas0016@gmail.com>
2025-03-25 12:41:38 -0700

    function in rust again pratices

---
+++ function_pra/target/debug/deps/function_pra.d
@@ -0,0 +1,5 @@
+C:\Users\Saqlain_abbas_313\OneDrive\Desktop\Rust_language\function_pra\target\debug\deps\function_pra.exe: src\main.rs
+
+C:\Users\Saqlain_abbas_313\OneDrive\Desktop\Rust_language\function_pra\target\debug\deps\function_pra.d: src\main.rs
+
+src\main.rs:
---
+++ function_pra/target/debug/function_pra.d
@@ -0,0 +1,1 @@
+C:\Users\Saqlain_abbas_313\OneDrive\Desktop\Rust_language\function_pra\target\debug\function_pra.exe: C:\Users\Saqlain_abbas_313\OneDrive\Desktop\Rust_language\function_pra\src\main.rs
