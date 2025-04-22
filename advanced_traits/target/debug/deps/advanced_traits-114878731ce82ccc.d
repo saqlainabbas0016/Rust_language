b61257abb42e0a700f51f1e5a1593389cfbd7185
saqlain <saqlainabbas0016@gmail.com>
2025-04-22 13:45:56 +0500

    advance

---
+++ advanced_traits/target/debug/deps/advanced_traits-114878731ce82ccc.d
@@ -0,0 +1,5 @@
+C:\Users\Saqlain_abbas_313\OneDrive\Desktop\rust_topics-main\advanced_traits\target\debug\deps\libadvanced_traits-114878731ce82ccc.rmeta: src\main.rs
+
+C:\Users\Saqlain_abbas_313\OneDrive\Desktop\rust_topics-main\advanced_traits\target\debug\deps\advanced_traits-114878731ce82ccc.d: src\main.rs
+
+src\main.rs:
---
+++ advanced_traits/target/debug/deps/advanced_traits-e1fbfe4d25ed8b4e.d
@@ -0,0 +1,5 @@
+C:\Users\Saqlain_abbas_313\OneDrive\Desktop\rust_topics-main\advanced_traits\target\debug\deps\libadvanced_traits-e1fbfe4d25ed8b4e.rmeta: src\main.rs
+
+C:\Users\Saqlain_abbas_313\OneDrive\Desktop\rust_topics-main\advanced_traits\target\debug\deps\advanced_traits-e1fbfe4d25ed8b4e.d: src\main.rs
+
+src\main.rs:
